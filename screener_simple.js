3f08052da7546bc395fa6885f5a9e8cf967bac97
daten88 <davidten1988@gmail.com>
2026-05-02 19:19:27 +0700

    Update WATCHLIST with new stock symbols

--- screener_simple.js
+++ screener_simple.js
@@ -30,13 +30,13 @@ const axios = require('axios');
 const TI    = require('technicalindicators'); // sama persis seperti complex screener
 
 // ── Config ────────────────────────────────────────────────────────────────────
-const WATCHLIST = ['AADI','AHAP','ARCI','ASPR','ATAP','BBRI','BBTN','BFIN','BGTG','BIPI',
-'BMRI','BNBR','BRMS','BRPT','BULL','BUMI','BUVA','BWPT','COCO','CTTH',
-'CUAN','DATA','DEWA','DKFT','ELSA','EMAS','EMTK','ENRG','ESSA','FORE',
-'GOTO','GTSI','HUMI','IMPC','INCO','INDY','JPFA','KETR','KPIG','MBMA',
-'MBSS','MDKA','MINA','NINE','PADA','PADI','PANI','PPRE','PSAT','PSKT',
-'PTPP','PYFA','RAJA','SINI','SOFA','SUPA','TAPG','TKIM','TPIA','TRIN',
-'TRUE','VKTR','WIFI','WMUU','ZATA'
+const WATCHLIST = ['AADI','AHAP','ARCI','ASPR','ATAP','BAIK','BBRI','BBTN','BFIN','BGTG',
+'BIPI','BMRI','BNBR','BRMS','BRPT','BULL','BUMI','BUVA','BWPT','COCO',
+'CTTH','CUAN','DATA','DEWA','DKFT','ELSA','EMAS','EMTK','ENRG','ESIP',
+'ESSA','FORE','GOTO','GTSI','HUMI','ICON','IMPC','INCO','INDY','JPFA',
+'KETR','KING','KPIG','MBMA','MBSS','MDKA','MINA','NINE','PADA','PADI',
+'PANI','PPRE','PSAT','PSKT','PTPP','PYFA','RAJA','SINI','SOFA','SUPA',
+'TAPG','TKIM','TPIA','TRIN','TRUE','VKTR','WIFI','WMUU','ZATA'
 ];
 
 const CFG = {

a4e1e9775ae1550e82ce7381d5e7ec2165620891
daten88 <davidten1988@gmail.com>
2026-05-02 20:52:34 +0700

    Update screener_simple.js

--- screener_simple.js
+++ screener_simple.js
@@ -30,15 +30,13 @@ const axios = require('axios');
 const TI    = require('technicalindicators'); // sama persis seperti complex screener
 
 // ── Config ────────────────────────────────────────────────────────────────────
-const WATCHLIST = ['AADI','ADRO','AHAP','ARCI','ASPR','ATAP','BAIK','BBRI','BBTN','BBYB',
-'BFIN','BGTG','BIPI','BMRI','BNBR','BRMS','BRPT','BULL','BUMI','BUVA',
-'BWPT','CASH','COCO','CPRO','CTTH','CUAN','DATA','DEWA','DKFT','ELSA',
-'ELTY','EMAS','EMTK','ENRG','ESIP','ESSA','FORE','GOTO','GTSI','HUMI',
-'ICON','IMPC','INCO','INDY','JGLE','JKON','JPFA','KBLV','KETR','KING',
-'KPIG','LEAD','MBMA','MBSS','MDKA','MINA','NINE','PACK','PADA','PADI',
-'PANI','PPRE','PSAT','PSKT','PTPP','PYFA','RAJA','SINI','SOCI','SOFA',
-'SUPA','TAPG','TKIM','TPIA','TOBA','TRIN','TRUE','VKTR','WIFI','WMUU',
-'ZATA'
+const WATCHLIST = ['AADI','AHAP','ARCI','ASPR','BBRI','BBYB','BIPI','BMRI','BNBR','BRMS',
+'BRPT','BULL','BUMI','BUVA','BWPT','CARE','CASH','COCO','CTTH','CUAN',
+'DATA','DEWA','DKFT','ELSA','EMAS','ESIP','ESSA','FORE','GOTO','GTSI',
+'HUMI','ICON','IMPC','INDY','KETR','KING','KPIG','MAIN','MBMA','MBSS',
+'MDKA','MINA','NAYZ','NINE','PADA','PADI','PANI','PPRE','PSKT','PTPP',
+'PYFA','RAJA','SOFA','SUPA','TAPG','TKIM','TPIA','TRIN','TRUE','VKTR',
+'WIFI','WMUU','YELO','ZATA'
 ];
 
 const CFG = {
